1880a032efdb6b6a6be79c6bb6939706d63e2b27
Brett Hobbs <brett@houzz.com>
2019-06-29 10:03:06 -0700

    Add hours since parameter

--- scripts/lever_api.js
+++ scripts/lever_api.js
@@ -2,49 +2,49 @@ import _ from 'lodash';
 import fetch from 'node-fetch';
 
 export default class LeverApi {
-    constructor(auth) {
-        this.auth = auth;
+  constructor(auth) {
+    this.auth = auth;
+  }
+
+  async fetchLeverData(apiUrl) {
+    return fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8;',
+        Authorization: `Basic ${this.auth}`,
+      },
+    })
+      .then(res => res.json())
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  async fetchStagesMap() {
+    const LIST_STAGES = 'https://api.lever.co/v1/stages';
+
+    return this.fetchLeverData(LIST_STAGES)
+      .then(({ data }) => _.reduce(data, (stagesMap, stage) => {
+        stagesMap[stage.id] = stage.text;
+        return stagesMap;
+      }, {}));
+  }
+
+  async fetchCandidatesUpdatedSinceTimestamp(postingId, timestamp) {
+    const LIST_CANDIDATES = `https://api.lever.co/v1/candidates?expand=owner&posting_id=${postingId}&updated_at_start=${timestamp}`;
+
+    let { data, hasNext, next } = await this.fetchLeverData(LIST_CANDIDATES);
+    let candidates = data;
+    while (hasNext) {
+      const listCandidates = `${LIST_CANDIDATES}&offset=${next}`;
+      // Block inside this loop since this makes an API call.
+      // eslint-disable-next-line no-await-in-loop
+      ({ data, hasNext, next } = await this.fetchLeverData(listCandidates));
+      candidates = [
+        ...candidates,
+        ...data,
+      ];
     }
-
-    async fetchLeverData(apiUrl) {
-        return fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8;',
-                Authorization: `Basic ${this.auth}`,
-            },
-        })
-            .then(res => res.json())
-            .catch((err) => {
-                console.error(err);
-            });
-    }
-
-    async fetchStagesMap() {
-        const LIST_STAGES = 'https://api.lever.co/v1/stages';
-
-        return this.fetchLeverData(LIST_STAGES)
-            .then(({ data }) => _.reduce(data, (stagesMap, stage) => {
-                stagesMap[stage.id] = stage.text;
-                return stagesMap;
-            }, {}));
-    }
-
-    async fetchCandidates(postingId, timestamp) {
-        const LIST_CANDIDATES = `https://api.lever.co/v1/candidates?expand=owner&posting_id=${postingId}&created_at_start=${timestamp}`;
-
-        let { data, hasNext, next } = await this.fetchLeverData(LIST_CANDIDATES);
-        let candidates = data;
-        while (hasNext) {
-            const listCandidates = `${LIST_CANDIDATES}&offset=${next}`;
-            // Block inside this loop since this makes an API call.
-            // eslint-disable-next-line no-await-in-loop
-            ({ data, hasNext, next } = await this.fetchLeverData(listCandidates));
-            candidates = [
-                ...candidates,
-                ...data,
-            ];
-        }
-        return candidates;
-    }
+    return candidates;
+  }
 }
--- scripts/sync_candidates.js
+++ scripts/sync_candidates.js
@@ -5,59 +5,65 @@ import LeverApi from './lever_api';
 
 // Command line arguments
 const args = minimist(process.argv.slice(2));
-const {
+
+async function main() {
+  // Move from to descriptive arguments
+  const {
     // Lever
     l: leverAuth,
     p: postingIdsCommaSperated,
-    t: timestamp,
+    t: timest, // create since timestamp
+    hours_since_updated: hoursSinceUpdated,
     // Coda
     c: codaAuth,
     d: docId,
     g: tableId,
-} = args;
+  } = args;
 
-async function main() {
-    const lever = new LeverApi(leverAuth);
+  const lever = new LeverApi(leverAuth);
+  // Clean this up, but --t is good for back filling and --hours_since_updated is good for scheduled updates
+  const timestamp = timest || Date.now() - hoursSinceUpdated * 3600000;
+  const stagesMap = await lever.fetchStagesMap();
+  const postingIds = postingIdsCommaSperated.split(',');
+  const fetchPromises = [];
+  _.forEach(postingIds, (pid) => {
+    fetchPromises.push(lever.fetchCandidatesUpdatedSinceTimestamp(pid, timestamp));
+  });
+  const results = await Promise.all(fetchPromises);
 
-    const stagesMap = await lever.fetchStagesMap();
-    const postingIds = postingIdsCommaSperated.split(',');
-    const fetchPromises = [];
-    _.forEach(postingIds, (pid) => {
-        fetchPromises.push(lever.fetchCandidates(pid, timestamp));
-    });
-    const results = await Promise.all(fetchPromises);
-
-    const leverCandidates = [];
-    _.forEach(results, (candidates) => {
-        leverCandidates.push(...candidates);
-    });
-
-    const flatCandidates = _.map(leverCandidates, c => ({
-        name: c.name,
-        id: c.id,
-        origin: c.origin,
-        sources: c.sources.join(','),
-        owner: c.owner.name,
-        created: new Date(c.createdAt),
-        advanced: new Date(c.lastAdvancedAt),
-        'last interaction': new Date(c.lastInteractionAt),
-        stage: stagesMap[c.stage],
-        url: c.urls.show,
-    }));
-
-    console.log(`Found ${leverCandidates.length} candidates`);
-
-    if (flatCandidates.length > 0) {
-        const coda = new Coda(codaAuth);
-        const doc = await coda.getDoc(docId);
-        const table = await doc.getTable(tableId);
-        const keyColumns = ['id'];
-        const upsertPromises = [];
-        _.forEach(_.chunk(flatCandidates, 100), (chunk) => {
-            upsertPromises.push(table.insertRows(chunk, keyColumns));
-        });
-        await Promise.all(upsertPromises);
+  const leverCandidates = [];
+  _.forEach(results, (candidates) => {
+    if (candidates) {
+      leverCandidates.push(...candidates);
     }
+  });
+
+  const flatCandidates = _.map(leverCandidates, c => ({
+    name: c.name,
+    id: c.id,
+    origin: c.origin,
+    sources: c.sources.join(','),
+    owner: c.owner.name,
+    created: new Date(c.createdAt),
+    advanced: new Date(c.lastAdvancedAt),
+    'last interaction': new Date(c.lastInteractionAt),
+    stage: stagesMap[c.stage],
+    url: c.urls.show,
+  }));
+
+  console.log(`Found ${leverCandidates.length} candidates`);
+
+  if (flatCandidates.length > 0) {
+    const coda = new Coda(codaAuth);
+    const doc = await coda.getDoc(docId);
+    const table = await doc.getTable(tableId);
+    const keyColumns = ['id'];
+    const upsertPromises = [];
+    _.forEach(_.chunk(flatCandidates, 100), (chunk) => {
+      upsertPromises.push(table.insertRows(chunk, keyColumns));
+    });
+    await Promise.all(upsertPromises);
+  }
 }
 
 main();
